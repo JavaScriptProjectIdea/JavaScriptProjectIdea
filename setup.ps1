99e64f376f6d69652e631b38c1e431da986fc857
Abdelwaheb AMMAR <aammar@ige-xao.com>
2025-06-03 00:25:46 +0100

    Workspace with 3 JavaScript projects

--- setup.ps1
+++ setup.ps1
@@ -1,9 +1,6 @@
-#!/bin/bash
-mkdir -p repos
-cd repos
+mkdir JavaScriptProjectIdea
+cd JavaScriptProjectIdea
 
-
-# Cloner tous les repos nécessaires
 git clone https://github.com/JavaScriptProjectIdea/Day-of-Week.git
 git clone https://github.com/JavaScriptProjectIdea/Word-Counter.git
 git clone https://github.com/JavaScriptProjectIdea/Tip-Calculator.git
