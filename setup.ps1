90e1c754ff2c3011bb9c192f16d2a0a0dc3d5796
Abdelwaheb AMMAR <aammar@ige-xao.com>
2025-06-03 00:09:21 +0100

    Initial commit

--- setup.ps1
+++ setup.ps1
@@ -2,6 +2,7 @@
 mkdir -p repos
 cd repos
 
+
 # Cloner tous les repos nécessaires
 git clone https://github.com/JavaScriptProjectIdea/Day-of-Week.git
 git clone https://github.com/JavaScriptProjectIdea/Word-Counter.git
