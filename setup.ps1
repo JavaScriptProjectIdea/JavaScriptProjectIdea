1d12100eb36a9d51ce0e94be73263db1bec93ca0
Abdelwaheb AMMAR <aammar@ige-xao.com>
2025-06-06 19:23:34 +0100

    update on configuration projects

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,6 @@
 mkdir JavaScriptProjectIdea
 cd JavaScriptProjectIdea
-
+git clone https://github.com/JavaScriptProjectIdea/Number-Guessing-Game.git
 git clone https://github.com/JavaScriptProjectIdea/Day-of-Week.git
 git clone https://github.com/JavaScriptProjectIdea/Word-Counter.git
 git clone https://github.com/JavaScriptProjectIdea/Tip-Calculator.git
